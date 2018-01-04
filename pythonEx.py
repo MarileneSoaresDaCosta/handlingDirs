19b64383c6f9977d7295d1355ac202dd7a6065bf
MarileneSoaresDaCosta <dacosta@alumni.harvard.edu>
2018-01-03 18:57:11 -0800

    adding files

---
+++ pythonEx.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# Marilene Soares da Costa - Script
+'''
+Assumptions and clarifications:
+
+- the script below and the unit test (test_PythonEx.py) is written in Python 2.7.13
+- the user will enter a string or a complete regular expression (regex) 
+to be located within the contents of all files in the chosen root_dir. 
+- Once a the first match is found in a file, that is counted as 1 (success).
+- The final output is the number of successful matches (a maximum of
+one per file) for each subdirectory including the root_dir.
+- The output is shown in two forms:
+- - as a Python dictionary where every key is a subdirectory path (including
+the root_dir), and corresponding values are the number of times the keyword
+was found on a file in that subdirectory.
+- - as a graph where each X corresponds to as subdir name string, and each
+corresponding Y represents the count values. 
+
+'''
+import sys
+import os
+import glob
+import re
+import mmap
+import matplotlib.pyplot as plt
+
+# create dict to store overall results
+output = {}
+
+# traverse the directory, calling searchSubdir 
+def walk(root_dir, keyword):
+	# compiles regex into a pattern object to use re.search later
+	reKey = re.compile(keyword)
+	# traverse directories
+	for path, subdirs, files in os.walk(root_dir):
+		output[path] = searchSubdir(path, reKey)
+	return output
+
+# counts in how many files, in each subdir, the keyword occurs at least once
+def searchSubdir(subdir, reKey):
+	# create dict to store results for each subdir 
+	matches = {}
+	count = 0
+    # using glob to list directories while ignoring hidden files
+	for file in glob.glob(os.path.join(subdir, '*')):
+		# check if it is a file, and that is not empty (avoids Windows error) 
+		if os.path.isfile(file) and os.stat(file).st_size != 0:
+			with open(file) as f:
+				# Memory-map files instead of reading the contents directly
+				m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+				if re.search(reKey, m):
+					count += 1
+					m.close()
+
+	matches[subdir] = count
+	return matches[subdir]
+
+# splits each directory string into one subdir per line for readability
+def split_dir(str):
+	sList = str.split("/")
+	return '-\n'.join(sList[i] for i in range(0, len(sList)))
+
+# generate 2nd required output (graph)
+def Graph(outuput):
+	plt.bar(range(len(output)), output.values(), align='center')
+	plt.xticks(range(len(output)), [split_dir(i) for i in output.keys()])
+	plt.show()
+
+def isDirInCWD(root_dir):
+	if os.path.isdir(root_dir): return True
+	
+
+
+
+
+if __name__ == '__main__':
+	# getting use input and testing if root_dir is in cwd
+	root_dir = raw_input('Enter the root directory: ')
+	if isDirInCWD(root_dir):
+		keyword = raw_input('Enter keyword or regex to search: ')
+	else:
+		print 'this directory is not in the cwd.'
+		restart = raw_input('Restart script? y or n: ')
+		if restart == 'y':
+			os.execl(sys.executable, sys.executable, *sys.argv)
+		else:
+			print 'Terminating the script.'
+			exit()
+
+
+	# generate 1st required output (Python dict)
+	print walk(root_dir, keyword)
+
+	# generate 2nd required output (graph)
+	Graph(output)
+
---
+++ test_pythonEx.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# Marilene Soares da Costa - Python Exercise - Tests
+
+import unittest
+import os
+import sys
+import glob
+import shutil
+import pythonEx
+
+
+class TestPythonEx(unittest.TestCase):
+
+	def createDir(name):
+		if os.path.isdir(name):
+			print 'this dir already exists'
+		else:
+			print 'creating new dir'
+			os.mkdir(name)
+			if os.path.isdir(name):
+				print 'now the dir has been created'
+
+	def setUp(self):
+		'''
+		creates a test directory with files with and without keywords
+		'''
+		self.rootName = 'testDirRoot'
+		if not os.path.isdir(self.rootName): 
+			os.mkdir(self.rootName)
+
+		# defining names of subdirs
+		subdirList = []
+		for i in range(1,4):
+			subdirList.append(self.rootName + "/sub" + str(i))
+		for subdir in subdirList:
+			#createDir(subdir)
+			if not os.path.isdir(subdir): 
+				os.mkdir(subdir)
+
+		not_keywords = "string for all files"
+		keywords = "alpha_TESTResult.txt caterpillar CaTeRpIlLaR"
+
+		# creates files with keywords
+		k = 5
+		for subdir in subdirList:
+			for i in range(1, k):
+				filename = "haskeys" + str(i) + ".txt"
+				filepath = os.path.join(subdir, filename)
+				with open(filepath, "w") as f:
+					f.write(keywords)
+					f.close
+				filename = "haskeys"
+			k += 1
+
+		# creates files without keywords
+		n = 4
+		for subdir in subdirList:
+			for i in range(1, n):
+				filename = "nokeys" + str(i) + ".txt"
+				filepath = os.path.join(subdir, filename)
+				with open(filepath, "w") as f:
+					f.write(not_keywords)
+					f.close
+				filename = "nokeys"
+			n += 1
+
+		# create one empty directory under the root
+		empdir = self.rootName + "/Empty"
+		if not os.path.isdir(empdir): 
+				os.mkdir(empdir)
+
+	# helper function
+	def removeDir(self, path):
+			if os.path.isdir(path):
+				shutil.rmtree(path, ignore_errors=True)
+			else:
+				print "Not a valid directory"
+
+	
+	# testing function walk
+	def test_simpleString(self):
+		expectedOutput = {'testDirRoot':0, 'testDirRoot/sub1': 4, 'testDirRoot/sub2': 5,'testDirRoot/sub3': 6, 'testDirRoot/Empty': 0}	
+		self.assertEqual(pythonEx.walk(self.rootName, 'caterpillar'), expectedOutput)
+
+	def test_regex1(self):
+		expectedOutput = {'testDirRoot':0, 'testDirRoot/sub1': 4, 'testDirRoot/sub2': 5,'testDirRoot/sub3': 6, 'testDirRoot/Empty': 0}	
+		self.assertEqual(pythonEx.walk(self.rootName, '(?i)(caterpillar)'), expectedOutput)
+
+
+	def test_regex2(self):
+		expectedOutput = {'testDirRoot':0, 'testDirRoot/sub1': 4, 'testDirRoot/sub2': 5,'testDirRoot/sub3': 6, 'testDirRoot/Empty': 0}	
+		self.assertEqual(pythonEx.walk(self.rootName, "^[a-zA-Z]+_TESTResult.*"), expectedOutput)
+
+	def main():
+		unittest.main()
+
+if __name__ == '__main__':
+	unittest.main()
